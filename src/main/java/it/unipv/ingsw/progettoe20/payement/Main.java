a348185e1197938638e9f0a21611133cf0ed8727
cedric-perez <65092910+cedric-perez@users.noreply.github.com>
2020-06-07 22:03:45 +0200

    creation of parkage it.unipv.ingsw.progettoe20.payement

---
+++ src/main/java/it/unipv/ingsw/progettoe20/payement/Main.java
@@ -0,0 +1,16 @@
+package it.unipv.ingsw.progettoe20.payement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int hour =6;
+		double price_per_hour= 2.5;
+		double price=hour*price_per_hour;
+		
+		Payment p1=new Payment(price);
+		p1.setVisible(true);
+		
+
+	}
+
+}
