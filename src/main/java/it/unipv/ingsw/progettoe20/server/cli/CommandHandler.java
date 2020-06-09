ba56ba88851a782776801b77ba5b0ad387a19eab
Giulia <giulia.testa01@universitadipavia.it>
2020-06-09 17:51:26 +0200

    Soluzione errore dovuto al cambio di nome dell'Administrator Signed-off-by: Giulia <giulia.testa01@universitadipavia.it>

--- src/main/java/it/unipv/ingsw/progettoe20/server/cli/CommandHandler.java
+++ src/main/java/it/unipv/ingsw/progettoe20/server/cli/CommandHandler.java
@@ -1,27 +1,27 @@
 package it.unipv.ingsw.progettoe20.server.cli;
 
 import it.unipv.ingsw.progettoe20.server.Logger;
-import it.unipv.ingsw.progettoe20.server.admin.model.Administrator;
+import it.unipv.ingsw.progettoe20.server.admin.model.AdministratorFactory;
 import it.unipv.ingsw.progettoe20.server.admin.view.AdministratorGUI;
 import it.unipv.ingsw.progettoe20.server.database.DatabaseFacade;
 
 class CommandHandler {
-    static void handle(String command) {
-        switch (command) {
-            // A friendly pin
-            case (CommandStrings.PING):
-                System.out.println(CommandStrings.PONG);
-                break;
-            // Admin GUI requested
-            case (CommandStrings.GUI):
-                Administrator.create(DatabaseFacade.getInstance());
-        		AdministratorGUI adminGui = new AdministratorGUI();
-                break;
-            // Server stop requested
-            case (CommandStrings.EXIT):
-                System.out.println(CommandStrings.EXIT_REQUESTED);
-                Logger.close();
-                System.exit(0);
-        }
-    }
+	static void handle(String command) {
+		switch (command) {
+		// A friendly pin
+		case CommandStrings.PING:
+			System.out.println(CommandStrings.PONG);
+			break;
+		// Admin GUI requested
+		case CommandStrings.GUI:
+			AdministratorFactory.create(DatabaseFacade.getInstance());
+			AdministratorGUI adminGui = new AdministratorGUI();
+			break;
+		// Server stop requested
+		case CommandStrings.EXIT:
+			System.out.println(CommandStrings.EXIT_REQUESTED);
+			Logger.close();
+			System.exit(0);
+		}
+	}
 }
