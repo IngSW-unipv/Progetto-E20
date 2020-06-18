6371ea4d950ab03c5dcfcbd7bda44771c8906f9d
pusi77 <andrea.pusineri01@universitadipavia.it>
2020-06-18 22:59:56 +0200

    refactored PaymentCalculator

--- src/main/java/it/unipv/ingsw/progettoe20/server/PaymentCalculator.java
+++ src/main/java/it/unipv/ingsw/progettoe20/server/PaymentCalculator.java
@@ -9,28 +9,26 @@ import java.util.List;
 /**
  * Contiene il metodo per calcolare il pagamento
  */
-
 public class PaymentCalculator {
 
     /**
      * Metodo che fa il calcolo del prezzo dato un biglietto e una lista delle tariffe.
      *
-     * @param ticket il biglietto.
+     * @param ticket    il biglietto.
      * @param pricelist lista delle tariffe.
      * @return il prezzo da pagare.
      */
     public static double getPaymentAmount(Ticket ticket, List<Price> pricelist) {
         Collections.sort(pricelist);
-        double amount = 0.0;
-            if (ticket.TimeDiff() <= pricelist.get(0).getMinutes()) {
-                amount = pricelist.get(0).getPrice();
+        double amount;
+        if (ticket.TimeDiff() <= pricelist.get(0).getMinutes()) {
+            amount = pricelist.get(0).getPrice();
 
-            } else if(ticket.TimeDiff() >= pricelist.get(2).getMinutes()){
-                amount = pricelist.get(2).getPrice();
-            }
-            else{
-                amount = pricelist.get(1).getPrice() * Math.ceil(ticket.TimeDiff()/60);
-            }
-            return amount;
+        } else if (ticket.TimeDiff() >= pricelist.get(2).getMinutes()) {
+            amount = pricelist.get(2).getPrice();
+        } else {
+            amount = pricelist.get(1).getPrice() * Math.ceil(ticket.TimeDiff() / 60);
+        }
+        return amount;
     }
 }
